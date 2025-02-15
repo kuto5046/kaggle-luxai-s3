3e611d99d4327b5dd2a60d5f5c36925b3f6d2770
Kyohei Uto <h27.kubb@gmail.com>
2025-02-15 07:47:24 +0000

    fix

--- exp/exp017/lux/models.py
+++ exp/exp017/lux/models.py
@@ -227,7 +227,8 @@ class LaxLitModel(LightningModule):
 
         policy_preds = torch.softmax(outputs["policy"], dim=1)
         policy_targets = one_hot_encoder(batch["action"], n_classes=len(Action))
-        policy_loss = self.criterion1(policy_preds, policy_targets)
+        policy_mask = (batch["state"][:, -1, State.OWN_UNIT_COUNT] > 0).unsqueeze(1)  # (batch_size, 1, w, h)
+        policy_loss = self.criterion1(policy_preds, policy_targets, policy_mask)
 
         # value_loss = self.criterion2(outputs["value"].flatten(), batch["win"])
         state_loss = self.criterion3(outputs["state"].flatten(), batch["hidden_state"].flatten())
