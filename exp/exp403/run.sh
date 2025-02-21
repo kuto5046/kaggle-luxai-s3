60929d19f80b81cc61b839edd2da230b37c344c5
KawattaTaido <mochijmdchj@gmail.com>
2025-02-21 19:40:01 +0900

    copy 400 to 403

---
+++ exp/exp403/run.sh
@@ -0,0 +1,12 @@
+# uv run python train.py
+# uv run python train.py --res true --notes "res"
+# uv run python train.py --aug true --notes "aug"
+# uv run python train.py --n_stack 1 --notes "n_stack 1"
+# uv run python train.py --loss_weight_opp_policy 1.0 --notes "loss_weight_opp_policy 1.0"
+# uv run python train.py --loss_weight_state 1.0 --notes "loss_weight_state 1.0"
+# uv run python train.py --loss_weight_global_state 1.0 --notes "loss_weight_global_state 1.0"
+# uv run python train.py --res true --loss_weight_state 1.0 --loss_weight_global_state 1.0 --notes "best params"
+# uv run python train.py --res true --aug true --loss_weight_state 1.0 --loss_weight_global_state 1.0 --notes "best params"
+# uv run python train.py --res true --loss_weight_state 1.0 --loss_weight_global_state 1.0 --notes "best params aDg4b"
+# uv run python train.py --res true --aug true --loss_weight_state 1.0 --loss_weight_global_state 1.0 --notes "best params with aug aDg4b"
+uv run python train.py --res true --loss_weight_state 1.0 --loss_weight_global_state 1.0 --limit_train_batches 0.1 --notes "best params aDg4b"
