4b360e61848d3748bd310e755458892ded355fdd
KawattaTaido <mochijmdchj@gmail.com>
2025-02-21 19:39:12 +0900

    extract_hidden_global_stateを修正してみたがあまりlossは改善せず

--- exp/exp401/lux/utils.py
+++ exp/exp401/lux/utils.py
@@ -51,8 +51,8 @@ class HiddenGlobalState(IntEnum):
     UNIT_SAP_DROPOFF_FACTOR = auto()
     UNIT_ENERGY_VOID_FACTOR = auto()
     # NEBULA_TILE_DRIFT_SPEED = auto() . # 推定可能なので不要
-    ENERGY_NODE_DRIFT_SPEED = auto()  # 推定可能だがあまり意味がなさそうなので Auxilary Lossのままにしてある
-    ENERGY_NODE_DRIFT_MAGNITUDE = auto()  # 多分推定できる
+    # ENERGY_NODE_DRIFT_SPEED = auto()  # 推定可能だがあまり意味がなさそうなので Auxilary Lossのままにしてある
+    # ENERGY_NODE_DRIFT_MAGNITUDE = auto()  # 多分推定できる
 
 
 class Action(IntEnum):
@@ -859,13 +859,19 @@ def extract_hidden_global_state(env_params: dict[str, Any]) -> np.ndarray:
 
 def extract_hidden_global_state2(params: dict[str, Any]) -> np.ndarray:
     hidden_global_states = np.zeros((len(HiddenGlobalState),), dtype=np.float32)
-    hidden_global_states[HiddenGlobalState.NEBULA_TILE_VISION_REDUCTION] = params["nebula_tile_vision_reduction"]
-    hidden_global_states[HiddenGlobalState.NEBULA_TILE_ENERGY_REDUCTION] = params["nebula_tile_energy_reduction"]
+    hidden_global_states[HiddenGlobalState.NEBULA_TILE_VISION_REDUCTION] = params["nebula_tile_vision_reduction"] / max(
+        env_params_ranges["nebula_tile_vision_reduction"]
+    )
+    hidden_global_states[HiddenGlobalState.NEBULA_TILE_ENERGY_REDUCTION] = params["nebula_tile_energy_reduction"] / max(
+        env_params_ranges["nebula_tile_energy_reduction"]
+    )
     hidden_global_states[HiddenGlobalState.UNIT_SAP_DROPOFF_FACTOR] = params["unit_sap_dropoff_factor"]
-    hidden_global_states[HiddenGlobalState.UNIT_ENERGY_VOID_FACTOR] = params["unit_energy_void_factor"]
+    hidden_global_states[HiddenGlobalState.UNIT_ENERGY_VOID_FACTOR] = (
+        params["unit_energy_void_factor"] / EnvParams.init_unit_energy
+    )
     # hidden_global_states[HiddenGlobalState.NEBULA_TILE_DRIFT_SPEED] = env_params.nebula_tile_drift_speed
-    hidden_global_states[HiddenGlobalState.ENERGY_NODE_DRIFT_SPEED] = params["energy_node_drift_magnitude"]
-    hidden_global_states[HiddenGlobalState.ENERGY_NODE_DRIFT_MAGNITUDE] = params["energy_node_drift_speed"]
+    # hidden_global_states[HiddenGlobalState.ENERGY_NODE_DRIFT_SPEED] = params["energy_node_drift_magnitude"]
+    # hidden_global_states[HiddenGlobalState.ENERGY_NODE_DRIFT_MAGNITUDE] = params["energy_node_drift_speed"]
     return hidden_global_states
 
 
