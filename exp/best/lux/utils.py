1858fa3aaf3202359f89dacc0680a714e48d39b8
okumura2997 <yukitora2015@gmail.com>
2025-02-23 10:23:49 +0900

    merge main

--- exp/best/lux/utils.py
+++ exp/best/lux/utils.py
@@ -358,9 +358,13 @@ class EpisodeStore:
             ]
 
             if len(candidates) == 0:
-                raise ValueError(
-                    f"矛盾が発生しているが条件を満たす候補が無い: {self.episode_id=} {steps=} {self._nebula_tile_drift_speed_candidates=}"
-                )
+                if self.validation:
+                    raise ValueError(
+                        f"矛盾が発生しているが条件を満たす候補が無い: {self.episode_id=} {steps=} {self._nebula_tile_drift_speed_candidates=}"
+                    )
+                else:
+                    # 矛盾が発生しているが条件を満たす候補が無い場合はスキップ(たまにある)
+                    return
 
             if self.validation:
                 # どちらかは一致しているはず
