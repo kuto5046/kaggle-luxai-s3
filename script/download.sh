5c4396bf65ec6f4a0930921ea3bcc1118c5474ce
okumura2997 <yukitora2015@gmail.com>
2025-02-23 21:44:03 +0900

    update

--- script/download.sh
+++ script/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python src/downloader.py --episode_path data/42778718/episodes.csv --output_dir data/42778718
+python src/downloader.py --episode_path data/42904799/episodes.csv --output_dir data/42904799
--- script/train.sh
+++ script/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 cd /kaggle
-python exp/exp605/train.py
+python exp/exp606/train.py
