a41eca557f4710994daeb3d76ea64b7a8d1ca68d
okumura2997 <yukitora2015@gmail.com>
2025-02-28 11:35:55 +0900

    update

--- script/download.sh
+++ script/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python src/downloader.py --episode_path data/42704976_0224/episodes.csv --output_dir data/42704976_0224
+python src/downloader.py --episode_path data/42704976_latest/episodes.csv --output_dir data/42704976_latest
