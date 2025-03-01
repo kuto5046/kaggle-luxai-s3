0e291e2fa7efb75a8d233f2a3a512b31b0224efe
chettub <khorie4900@gmail.com>
2025-03-01 19:51:46 +0900

    exp239 longer epochs

--- exp/exp239/train.py
+++ exp/exp239/train.py
@@ -34,7 +34,7 @@ class Config:
     output_dir = root_dir / f"exp/{exp_name}/output"
 
     # trainer
-    epoch: int = 30
+    epoch: int = 50
     limit_train_batches: float = 1.0
     limit_val_batches: float = 1.0
     use_amp: bool = True
