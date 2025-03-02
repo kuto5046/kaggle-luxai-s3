faa61e9ef055720c616c6a15c3810a04745419bd
kibuna <task.aizawa@gmail.com>
2025-03-02 16:55:46 +0900

    update dataloader config

--- exp/exp808/data_processor.py
+++ exp/exp808/data_processor.py
@@ -37,7 +37,7 @@ class Config:
     root_dir: Path = Path("/workspace/kaggle-luxai-s3")
     input_dir: Path = root_dir / "input"
     episode_dir: Path = root_dir / "output/feature_store/episodes"
-    episode_path: Path = episode_dir / "episodes0302.csv"
+    episode_path: Path = episode_dir / "episodes_0302.csv"
     feature_dir: Path = root_dir / f"output/feature_store/{exp_name}"
     target_team_name: str = "Frog Parade"
     target_sub_ids: list[int] = field(default_factory=lambda: [42704976, 42705163, 43152191, 43155694])
