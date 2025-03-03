9af2145c7a797d6eeffa3e61cdf70ceb384e0253
okumura2997 <yukitora2015@gmail.com>
2025-03-03 19:49:38 +0900

    update

--- script/eval.sh
+++ script/eval.sh
@@ -3,4 +3,4 @@
 agent1_path=$1
 agent2_path=$2
 
-uv run luxai-s3-fast $1 $2 --tournament --tournament-cfg-concurrent 1 --tournament-cfg-ranking-system wins
+uv run luxai-s3-fast $1 $2 --tournament --tournament-cfg-concurrent 4 --tournament-cfg-ranking-system wins
