9698373fdd41029ad86cc2a79012223274fbbc12
Kyohei Uto <h27.kubb@gmail.com>
2025-03-05 06:21:17 +0000

    exp032

--- exp/exp032/rl.py
+++ exp/exp032/rl.py
@@ -81,7 +81,7 @@ LB_BEST_POLICY = "lb_best"  # TODO: モデルや特徴量が異なるため未�
 @dataclass
 class Config:
     exp_name: str = Path(__file__).parent.name
-    is_gcp: bool = False
+    is_gcp: bool = True
     notes: str = "sap policyも合わせて学習する"
     env_name: str = "lux-s3-v0"
     n_stack: int = 4
@@ -824,9 +824,11 @@ class WandbLoggerCallback(RLlibCallback):
         self.logger.info(f"Evaluation {self._current_evaluation_id} completed episodes={eval_stats['total']}")
 
         # 評価結果をリセット
+        save_model(algorithm, self.output_dir, suffix=f"model_eval_{self._current_evaluation_id}")
+        save_model(algorithm, self.output_dir, suffix=f"latest_model")
         best_win_rate = ray.get(self._stats_collector.get_best_win_rate.remote())
         if best_win_rate < current_win_rate:
-            save_model(algorithm, self.output_dir, suffix=f"model_eval_{self._current_evaluation_id}")
+            save_model(algorithm, self.output_dir, suffix=f"best_model")
             self.logger.info(f"Best win rate updated. {best_win_rate=:.4f} -> {current_win_rate=:.4f}")
             # ベスト勝率を更新
             ray.get(self._stats_collector.update_best_win_rate.remote(current_win_rate))
