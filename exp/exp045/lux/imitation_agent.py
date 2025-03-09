9310f6baf9af5651033350a7055ca6271407ab1a
Kyohei Uto <h27.kubb@gmail.com>
2025-03-09 17:52:25 +0000

    fix

--- exp/exp045/lux/imitation_agent.py
+++ exp/exp045/lux/imitation_agent.py
@@ -20,7 +20,6 @@ from .utils import (
     get_legal_policy,
     calc_relative_pos,
     extract_global_state,
-    is_kaggle_environment,
 )
 from .params import EnvParams
 
@@ -211,11 +210,13 @@ class ILAgent:
 def use_cpp_flow():
     # 提出環境ではpythonのflow計算を使う想定. kaggle環境でpybind周りをなんとかすれば使えるはずだが未整備
     # debugモードではpythonのflow計算との比較を行うため使用している
-    return not is_kaggle_environment() or Config().debug
+    # return not is_kaggle_environment() or Config().debug
+    return False
 
 
 def use_py_flow():
-    return is_kaggle_environment() or Config().debug
+    # return is_kaggle_environment() or Config().debug
+    return True
 
 
 if use_cpp_flow():
