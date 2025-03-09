bed94586605a076fef040a5420ba69b4e9d09b07
Kyohei Uto <h27.kubb@gmail.com>
2025-03-09 01:52:06 +0000

    fix

--- exp/exp040/rl.py
+++ exp/exp040/rl.py
@@ -140,7 +140,7 @@ class Config:
     # learner
     gamma: float = 0.9995
     lr: float = 5e-5
-    train_batch_size_per_learner: int = 512
+    train_batch_size_per_learner: int = 256
     num_epochs: int = 1  # 1回の学習のepoch数。新しいデータがどんどん追加されてくるためepoch数は1にしている
     replay_proportion: float = 0.0  # リプレイバッファの割合
     # loss
