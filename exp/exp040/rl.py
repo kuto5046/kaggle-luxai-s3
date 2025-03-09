5bef7e43be7887cde81e00c1cc32cc394d809184
Kyohei Uto <h27.kubb@gmail.com>
2025-03-09 01:21:23 +0000

    fix path

--- exp/exp040/rl.py
+++ exp/exp040/rl.py
@@ -93,7 +93,7 @@ class Config:
     debug: bool = False
     notes: str = "best modelを反映してfreeze解除して学習"
     env_name: str = "lux-s3-v0"
-    root_dir: Path = Path("/home/user/work")
+    root_dir: Path = Path("/home/kyohei.uto/kaggle-luxai-s3")
     exp_dir: Path = root_dir / f"exp/{exp_name}"
     output_dir: Path = root_dir / f"output/{exp_name}"
 
