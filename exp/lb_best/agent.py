d0676fe4a95c72abdb14c54d6ec2f76ca33709e2
okumura2997 <yukitora2015@gmail.com>
2025-03-09 20:58:23 +0900

    fix

--- exp/lb_best/agent.py
+++ exp/lb_best/agent.py
@@ -40,7 +40,7 @@ class Config:
     tta: bool = True
     debug: bool = False
 
-    checkpoint_path: Path = Path(__file__).parent / "output/best_model_exp629.ckpt"
+    checkpoint_path: Path = Path(__file__).parent / "output/best_model.ckpt"
 
 
 ###########################################################################
